205d70cad5b1cdfae97a271cb0eb589e2205892d
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-16 10:55:41 +0200

    Handle "Add Todo Form" Submit in AddTodoForm.js

---
+++ react-todo/src/AddTodoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+const AddTodoForm = () => {
+  const [title, setTitle] = useState("")
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleAddTodo = (event) => {
+    event.preventDefault()
+    const todoTitle = event.target.elements.title.value // Retrieve the value of the title input field
+    console.log(todoTitle)
+    event.target.reset() // Reset the form to clear the text input
+  }
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input
+        type='text'
+        name='title'
+        value={title}
+        onChange={handleTitleChange}
+      />
+      <button type='submit'>Add Todo</button>
+    </form>
+  )
+}
+export default AddTodoForm
