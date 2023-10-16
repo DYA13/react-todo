2de434ceed75c22470998d5a8ad775e23947782e
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-16 11:04:52 +0200

    Store "New Todo" in React State

--- react-todo/src/AddTodoForm.js
+++ react-todo/src/AddTodoForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 
-const AddTodoForm = () => {
+const AddTodoForm = (props) => {
   const [title, setTitle] = useState("")
 
   const handleTitleChange = (event) => {
@@ -9,8 +9,11 @@ const AddTodoForm = () => {
 
   const handleAddTodo = (event) => {
     event.preventDefault()
+
     const todoTitle = event.target.elements.title.value // Retrieve the value of the title input field
     console.log(todoTitle)
+
+    props.onAddTodo(todoTitle)
     event.target.reset() // Reset the form to clear the text input
   }
   return (
