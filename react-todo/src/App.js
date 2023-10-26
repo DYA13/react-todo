ea74ef04a0196812e88ed56118c692773c29beee
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-26 11:28:20 +0200

    Add New Todo to List

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -6,16 +6,17 @@ function App() {
   // Step 1: Create a state variable for todoList
   const [todoList, setTodoList] = useState([])
 
-  // Step 3: Define the onAddTodo function to add new todos
-  const onAddTodo = (newTodo) => {
+  // Step 2: Declare the addTodo function
+  const addTodo = (newTodo) => {
+    // Step 2: Use the spread operator to update todoList with a new todo
     setTodoList([...todoList, { id: Date.now(), title: newTodo }])
   }
 
   return (
     <div>
       <h1>Todo List</h1>
-      {/* Step 4: Pass the onAddTodo function as a prop to AddTodoForm */}
-      <AddTodoForm onAddTodo={onAddTodo} />
+      {/* Step 3: Pass the addTodo function as a prop to AddTodoForm */}
+      <AddTodoForm onAddTodo={addTodo} />
       {/* Step 2: Pass todoList as a prop to TodoList */}
       <TodoList todoList={todoList} />
     </div>
