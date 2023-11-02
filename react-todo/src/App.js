92d6b8f1bca224ee32e70c57e90dc723ba7a939d
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-02 11:03:06 +0100

    save Todo List in Storage

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import TodoList from "./TodoList"
 import AddTodoForm from "./AddTodoForm"
 
 function App() {
-  // Step 1: Create a state variable for todoList
   const [todoList, setTodoList] = useState([])
 
-  // Step 2: Declare the addTodo function
   const addTodo = (newTodo) => {
-    // Step 2: Use the spread operator to update todoList with a new todo
     setTodoList([...todoList, { id: Date.now(), title: newTodo }])
   }
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList))
+  }, [todoList])
 
   return (
     <div>
       <h1>Todo List</h1>
-      {/* Step 3: Pass the addTodo function as a prop to AddTodoForm */}
+
       <AddTodoForm onAddTodo={addTodo} />
-      {/* Step 2: Pass todoList as a prop to TodoList */}
+
       <TodoList todoList={todoList} />
     </div>
   )
