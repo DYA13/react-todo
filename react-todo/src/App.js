9fa0012391301a03fe6fcbbb7ad3740ba10fdfdf
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-17 17:14:39 +0100

    Async and Add Loading State

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,35 +1,56 @@
-import React, { useState, useEffect } from "react"
-import TodoList from "./TodoList"
-import AddTodoForm from "./AddTodoForm"
+import React, { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  const key = "savedTodoList"
-  const initialState = []
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem(key)) || initialState
-  )
+  const key = "savedTodoList";
+
+  // Updated the default state for todoList to be an empty Array
+  const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(todoList))
-  }, [key, todoList])
+    const fetchData = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ data: { todoList: JSON.parse(localStorage.getItem(key)) || [] } });
+      }, 2000);
+    });
+
+    fetchData.then((result) => {
+      setTodoList(result.data.todoList);
+      setLoading(false); // Set isLoading to false after data fetch is complete
+    });
+  }, [key]);
+
   const addTodo = (newTodo) => {
-    setTodoList([...todoList, { id: Date.now(), title: newTodo }])
-  }
+    setTodoList([...todoList, { id: Date.now(), title: newTodo }]);
+  };
 
   const removeTodo = (id) => {
-    const updatedTodoList = todoList.filter((todo) => todo.id !== id)
-    setTodoList(updatedTodoList)
-  }
+    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(updatedTodoList);
+  };
+
+  useEffect(() => {
+    // Add an if statement to check that isLoading is false before setting localStorage
+    if (!loading) {
+      localStorage.setItem(key, JSON.stringify(todoList));
+    }
+  }, [key, loading, todoList]);
+
   return (
     <div>
       <h1>Todo List</h1>
-      <>
-        <AddTodoForm onAddTodo={addTodo} />
-
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-      </>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <AddTodoForm onAddTodo={addTodo} />
+          <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
