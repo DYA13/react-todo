ac48bf2719587e49095bb7a8c291975489304f47
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-17 17:04:52 +0100

    Remove Custom Hook useSemiPersistentState

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from "react"
 import TodoList from "./TodoList"
 import AddTodoForm from "./AddTodoForm"
-function useSemiPersistentState(key, initialState) {
-  const [value, setValue] = useState(
+
+function App() {
+  const key = "savedTodoList"
+  const initialState = []
+  const [todoList, setTodoList] = useState(
     JSON.parse(localStorage.getItem(key)) || initialState
   )
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
-  }, [key, value])
-
-  return [value, setValue]
-}
-function App() {
+    localStorage.setItem(key, JSON.stringify(todoList))
+  }, [key, todoList])
   const addTodo = (newTodo) => {
     setTodoList([...todoList, { id: Date.now(), title: newTodo }])
   }
-  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", [])
+
   const removeTodo = (id) => {
-    // Use the filter method to create a new array without the item with the given id.
     const updatedTodoList = todoList.filter((todo) => todo.id !== id)
     setTodoList(updatedTodoList)
   }
