a11bcb88bd220bd899ec7d981761430ee9090514
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-17 16:23:20 +0100

    Revert "Merge pull request #5 from DYA13/lesson_1_5"
    This reverts commit 6af4b3e6401df83e0ea6ab49c7a5a42932b9c307, reversing
    changes made to 363c588badde2da3aa7fd955008160f2732d416c.

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,31 +1,24 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import TodoList from "./TodoList"
 import AddTodoForm from "./AddTodoForm"
-function useSemiPersistentState(key, initialState) {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || initialState
-  )
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
-  }, [key, value])
-
-  return [value, setValue]
-}
 function App() {
+  // Step 1: Create a state variable for todoList
+  const [todoList, setTodoList] = useState([])
+
+  // Step 2: Declare the addTodo function
   const addTodo = (newTodo) => {
+    // Step 2: Use the spread operator to update todoList with a new todo
     setTodoList([...todoList, { id: Date.now(), title: newTodo }])
   }
-  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", [])
 
   return (
     <div>
       <h1>Todo List</h1>
-      <>
-        <AddTodoForm onAddTodo={addTodo} />
-
-        <TodoList todoList={todoList} />
-      </>
+      {/* Step 3: Pass the addTodo function as a prop to AddTodoForm */}
+      <AddTodoForm onAddTodo={addTodo} />
+      {/* Step 2: Pass todoList as a prop to TodoList */}
+      <TodoList todoList={todoList} />
     </div>
   )
 }
