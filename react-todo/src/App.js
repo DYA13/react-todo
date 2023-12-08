9a605a31510bdc3045927027fbf592bcfcfcdfd7
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-12-08 15:18:40 +0100

    refactoring

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import TodoList from "./TodoList"
 import AddTodoForm from "./AddTodoForm"
 
 function App() {
   const key = "savedTodoList"
 
+  // Step 1: Create a state variable for todoList
   const [todoList, setTodoList] = useState([])
   const [loading, setLoading] = useState(true)
+
+  // Step 2: Declare the addTodo function
+  const addTodo = (newTodo) => {
+    // Step 2: Use the spread operator to update todoList with a new todo
+    setTodoList([...todoList, { id: Date.now(), title: newTodo }])
+  }
+
+  const removeTodo = (id) => {
+    const updatedTodoList = todoList.filter((todo) => todo.id !== id)
+    setTodoList(updatedTodoList)
+  }
 
   useEffect(() => {
     const fetchData = new Promise((resolve, reject) => {
@@ -23,22 +35,6 @@ function App() {
     })
   }, [key])
 
-
-function App() {
-  // Step 1: Create a state variable for todoList
-  const [todoList, setTodoList] = useState([])
-
-  // Step 2: Declare the addTodo function
-  const addTodo = (newTodo) => {
-    // Step 2: Use the spread operator to update todoList with a new todo
-    setTodoList([...todoList, { id: Date.now(), title: newTodo }])
-  }
-
-  const removeTodo = (id) => {
-    const updatedTodoList = todoList.filter((todo) => todo.id !== id)
-    setTodoList(updatedTodoList)
-  }
-
   useEffect(() => {
     if (!loading) {
       localStorage.setItem(key, JSON.stringify(todoList))
@@ -48,23 +44,14 @@ function App() {
   return (
     <div>
       <h1>Todo List</h1>
+      {/* Step 3: Pass the addTodo function as a prop to AddTodoForm */}
       <AddTodoForm onAddTodo={addTodo} />
-
+      {/* Step 2: Pass todoList and onRemoveTodo as props to TodoList */}
       {loading ? (
         <p>Loading...</p>
       ) : (
         <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
       )}
-      </>
-
-  return (
-    <div>
-      <h1>Todo List</h1>
-      {/* Step 3: Pass the addTodo function as a prop to AddTodoForm */}
-      <AddTodoForm onAddTodo={addTodo} />
-      {/* Step 2: Pass todoList as a prop to TodoList */}
-      <TodoList todoList={todoList} />
-
     </div>
   )
 }
