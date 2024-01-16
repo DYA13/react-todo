920433ca2508cc6e5145bc34ace6f05bb6d6b5c6
Yelena Japarova <yelena.dzhaparova@gmail.com>
2024-01-16 11:14:56 +0100

    added PropTypes in TodoList.js

--- react-todo/src/components/TodoList.js
+++ react-todo/src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 import TodoListItem from "./TodoListItem"
 import style from "./App.module.css"
 
@@ -19,6 +20,15 @@ function TodoList({ todoList, onRemoveTodo }) {
       </ul>
     </>
   )
+}
+TodoList.propTypes = {
+  todoList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  onRemoveTodo: PropTypes.func.isRequired
 }
 
 export default TodoList
