e91fa08a2e149425baefc7865d6eb4304cc8c7ee
Yelena Japarova <yelena.dzhaparova@gmail.com>
2024-01-16 10:42:01 +0100

    Project Structure improved

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import TodoList from "./TodoList"
-import AddTodoForm from "./AddTodoForm"
+import TodoList from "./components/TodoList"
+import AddTodoForm from "./components/AddTodoForm"
 import style from "./App.module.css"
 function App() {
   const [todoList, setTodoList] = useState([])
