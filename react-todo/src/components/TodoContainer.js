a4aa88c71d87f1b63d96750697e00a87698c6225
Yelena Japarova <yelena.dzhaparova@gmail.com>
2024-01-30 15:37:50 +0100

    descending/ascending button

--- react-todo/src/components/TodoContainer.js
+++ react-todo/src/components/TodoContainer.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react"
 import TodoList from "./TodoList"
 import AddTodoForm from "./AddTodoForm"
 import style from "./App.module.css"
+import { AiOutlineSortAscending } from "react-icons/ai"
+import { AiOutlineSortDescending } from "react-icons/ai"
+
 const TodoContainer = () => {
   const [todoList, setTodoList] = useState([])
   const [loading, setLoading] = useState(true)
+  const [sortOrder, setSortOrder] = useState("asc")
 
   const airtableUrl = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`
   const airtableToken = process.env.REACT_APP_AIRTABLE_API_TOKEN
@@ -19,7 +23,7 @@ const TodoContainer = () => {
 
     try {
       const response = await fetch(
-        `${airtableUrl}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`,
+        `${airtableUrl}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=${sortOrder}`,
         options
       )
 
@@ -43,18 +47,18 @@ const TodoContainer = () => {
       //   return 0
       // })
       // Sorting in ascending order
-      data.records.sort((objectA, objectB) => {
-        const titleA = objectA.fields.title.toUpperCase()
-        const titleB = objectB.fields.title.toUpperCase()
+      // data.records.sort((objectA, objectB) => {
+      //   const titleA = objectA.fields.title.toUpperCase()
+      //   const titleB = objectB.fields.title.toUpperCase()
 
-        if (titleA < titleB) {
-          return -1
-        }
-        if (titleA > titleB) {
-          return 1
-        }
-        return 0
-      })
+      //   if (titleA < titleB) {
+      //     return -1
+      //   }
+      //   if (titleA > titleB) {
+      //     return 1
+      //   }
+      //   return 0
+      // })
       const todos = data.records.map((record) => {
         return { id: record.id, title: record.fields.title }
       })
@@ -64,6 +68,10 @@ const TodoContainer = () => {
     } catch (error) {
       console.error("Error fetching data:", error.message)
     }
+  }
+  const toggleSortOrder = () => {
+    const newSortOrder = sortOrder === "asc" ? "desc" : "asc"
+    setSortOrder(newSortOrder)
   }
 
   const addTodo = async (newTodo) => {
@@ -125,16 +133,24 @@ const TodoContainer = () => {
   useEffect(() => {
     fetchData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [sortOrder])
 
   return (
     <>
       <h1 className={style.h1}>Todo List</h1>
+
       <AddTodoForm onAddTodo={addTodo} />
       {loading ? (
         <p className={style.loading}>Loading ...</p>
       ) : (
         <div className={style.container}>
+          <button className={style.sortBtn} onClick={toggleSortOrder}>
+            {sortOrder === "asc" ? (
+              <AiOutlineSortAscending />
+            ) : (
+              <AiOutlineSortDescending />
+            )}
+          </button>
           <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
         </div>
       )}
